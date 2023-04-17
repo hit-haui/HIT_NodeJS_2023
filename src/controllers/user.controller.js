b245fb61d7e87a3f6b915ed45ab422c90c371913
unknown <lelenahkht2003@gamil.com>
2023-04-17 18:10:22 +0700

    fixed according to review

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,76 +1,33 @@
-const fs = require('fs')
-
-const path = require('path')
-
 const Users = require('../models/user.model')
 
 const getUsers = (req, res) => {
   const users = Users.find()
-  console.log(users)
   return res.json(users)
 }
 
 const getUserById = (req, res) => {
-  const users = Users.find()
   const { userId } = req.params
-  const user = Users.getById(userId)
-  if (!user) {
-    return res.json({ mgs: 'Not found' })
-  }
-  res.json(user)
+  const user = Users.getUserById(userId)
+  return res.json(user)
 }
 
 const createUser = (req, res) => {
   const data = req.body
-  const newUser = new Users(data)
-  newUser.save()
+  const newUser = Users.createUser(data)
   return res.json(newUser)
 }
 
 const updateUserById = (req, res) => {
-  const users = Users.find()
-  const { userId } = req.params
-  const index = Users.getIdIndex(userId)
-  if (index == -1) {
-    return res.json({ mgs: 'Not found user' })
-  }
   const data = req.body
-  const newUser = new Users(data)
-  users.splice(index, 1, newUser)
-  fs.writeFile(
-    path.join(__dirname, '../data/users.json'),
-    JSON.stringify(users),
-    (err) => {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log('Success')
-        res.json(users)
-      }
-    }
-  )
+  const { userId } = req.params
+  const users = Users.updateUserById(userId, data)
+  return res.json(users)
 }
 
 const deleteUserById = (req, res) => {
-  const users = Users.find()
   const { userId } = req.params
-  const index = Users.getIdIndex(userId)
-  if (index == -1) {
-    return res.json({ mgs: 'Not found to delete' })
-  }
-  users.splice(index, 1)
-  fs.writeFile(
-    path.join(__dirname, '../data/users.json'),
-    JSON.stringify(users),
-    (err) => {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log('Success')
-        res.json(users)
-      }
-    }
-  )
+  const users = Users.deleteUserById(userId)
+  res.json(users)
 }
 
 module.exports = {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,14 +59,65 @@ class Users {
     this.saveNewUser(users)
   }
 
-  static getById(id) {
+  static getUserById(id) {
     const users = Users.find()
-    return users.find((item) => item.id == id)
+    const user = users.find((item) => item.id == id)
+    if (!user) {
+      return 'Not found'
+    } else {
+      return user
+    }
   }
 
-  static getIdIndex(id) {
+  static createUser(data) {
+    const newUser = new Users(data)
+    newUser.save()
+    return newUser
+  }
+
+  static updateUserById(id, data) {
     const users = Users.find()
-    return users.findIndex((item) => item.id === id)
+    const index = users.findIndex((item) => item.id == id)
+    console.log(index)
+    if (index == -1) {
+      return 'Not found user to update'
+    } else {
+      const newUser = new Users(data)
+      users.splice(index, 1, newUser)
+      fs.writeFile(
+        path.join(__dirname, '../data/users.json'),
+        JSON.stringify(users),
+        (err) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Success')
+            return users
+          }
+        }
+      )
+    }
+  }
+
+  static deleteUserById(id) {
+    const users = Users.find()
+    const index = users.findIndex((item) => item.id == id)
+    if (index == -1) {
+      return 'Not found'
+    } else {
+      users.splice(index, 1)
+      fs.writeFile(
+        path.join(__dirname, '../data/users.json'),
+        JSON.stringify(users),
+        (err) => {
+          if (err) {
+            console.log(err)
+          } else {
+            return users
+          }
+        }
+      )
+    }
   }
 }
 

66fe7416edf6994e1aa495bebec6e5171b8624c0
caotrungduc <trungdc2003@gmail.com>
2023-04-19 17:06:39 +0700

    added file .env and dotenv library, fixed some bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,70 +1,58 @@
 const User = require("../models/user.model");
 
 // get users
-const getUsers = async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const getUsers = (req, res) => {
+  User.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ message: err.message }))
 };
 
 // get user by id
-const getUserById = async (req, res) => {
-  const userId = req.params.userId;
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found!" });
-    }
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+      return res.status(200).json(user);
+    })
+    .catch(err => res.status(500).json({ message: err.message }))
 };
 
 // create user
-const createUser = async (req, res) => {
-  const userRaw = req.body;
-  const newUser = new User(userRaw);
-  try {
-    await newUser.save();
-    res.status(201).json({ message: "Successfully created user!" });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+const createUser = (req, res) => {
+  const newUser = req.body;
+  User.create(newUser)
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(500).json({ message: err.message }))
 };
 
 // edit user information by id
-const updateUserById = async (req, res) => {
-  const userId = req.params.userId;
-  const userRaw = req.body;
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      res.status(404).json({ message: "User not found!" });
-    }
-    await User.updateOne({ _id: userId }, userRaw);
-    res.status(200).json({ message: "Successfully updated user information!" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const updateUserById = (req, res) => {
+  const { userId } = req.params;
+  const updateUser = req.body;
+  User.findByIdAndUpdate({ _id: userId }, updateUser)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+      return res.status(200).json({ message: "Successfully updated user information!" });
+    })
+    .catch(err => res.status(500).json({ message: err.message }))
 };
 
 // delete user by id
-const deleteUserById = async (req, res) => {
-  const userId = req.params.userId;
-  try {
-    const user = await User.find({ _id: userId });
-    if (!user) {
-      res.status(404).json({ message: "User not found" });
-    }
-    await User.deleteOne({ _id: userId });
-    res.status(200).json({ message: "Successfully delete user!" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const deleteUserById = (req, res) => {
+  const { userId } = req.params;
+  User.findByIdAndDelete({ _id: userId })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.status(204).json();
+    })
+    .catch(err => res.status(500).json({ message: err.message }))
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const router = require("./routes");
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+const router = require("./routes");
 
 const app = express();
 const port = 8080;
 
-mongoose.connect('mongodb://127.0.0.1:27017/users')
+mongoose.connect('mongodb://127.0.0.1:27017/users' || process.env.DATABASE_URL)
   .then(() => console.log('Connected!'))
   .catch(err => console.log(err.message))
 

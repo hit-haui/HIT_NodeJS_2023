2a07171c4c4bf4b57f9b0045a1432843b5aa8716
luong2k3dev <tunhatluong2k3@gmail.com>
2023-04-20 20:41:35 +0700

    CRUD Users API By MongoDB

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = require("./routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(router);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connect DB Successfully"))
+  .catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

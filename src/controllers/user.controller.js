6483dc49ac22ec0c2407e86a2ff4a1efdaf6b73b
Huong <hdnguyenthuhuong1234@gmail.com>
2023-04-21 22:34:21 +0700

    CRUD_User_withMongoose

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,76 +1,80 @@
 const User = require("../models/user.model");
-
-// get users
-const getUsers = (req, res) => {
-  // get all user
-  const users = User.getAllUser();
-  // return result
-  res.status(200).json({
-    users: users,
-  });
-};
-
-// get user by id
-const getUserById = (req, res) => {
-  const userId = req.params.userId;
-  // check user
-  const user = User.findUserByID(userId);
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found!",
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      users: users,
     });
+  } catch (err) {
+    conslog.log(err);
   }
-  // return result
-  res.status(200).json({
-    user: user,
-  });
 };
 
-// create user
-const createUser = (req, res) => {
-  // new user
-  const userRaw = req.body;
-  const newUser = new User(userRaw);
-  // add new user to file
-  newUser.addUser();
-  res.status(201).json({
-    message: "Successfully created user!",
-  });
-};
-
-// edit user information by id
-const updateUserById = (req, res) => {
-  const userId = req.params.userId;
-  // check user
-  const user = User.findUserByID(userId);
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found!",
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+      res.status(404).send({
+        message: "User not found",
+      });
+    }
+    res.status(200).json({
+      user,
     });
+  } catch (err) {
+    console.log(err);
   }
-  // update user
-  const userRaw = req.body;
-  User.updateUser(userId, userRaw);
-  res.status(200).json({
-    message: "Successfully updated user information!",
-  });
 };
 
-// delete user by id
-const deleteUserById = (req, res) => {
-  const userId = req.params.userId;
-  // check user
-  const user = User.findUserByID(userId);
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found!",
+const createUser = async (req, res) => {
+  const data = req.body;
+  try {
+    const newUser = new User(data);
+    await newUser.save();
+    res.status(200).json({
+      message: "A user has been created",
     });
+  } catch (err) {
+    console.log(err);
   }
-  // delete user
-  User.deleteUser(userId);
-  res.status(200).json({
-    message: "Successfully delete user!",
-  });
+};
+
+const updateUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updateUser = await User.findByIdAndUpdate(id, data);
+    if (!updateUser) {
+      res.status(404).json({
+        message: "User not found",
+      });
+    }
+    updateUser.save();
+    res.status(200).json({
+      message: "A user has been updated",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteUser = await User.findByIdAndDelete(id);
+    if (!deleteUser) {
+      res.status(404).json({
+        message: "User not found",
+      });
+    }
+    deleteUser.save();
+    res.status(200).json({
+      message: "A user has been deleted",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = require("./routes");
-
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 app.use(router);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/nodejs")
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.error(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

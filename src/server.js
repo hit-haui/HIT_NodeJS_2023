9f514d5ca75eabe96da51206d63d30601e4b797e
NgoThao2003 <thanhthaongo845@gmail.com>
2023-04-21 23:38:21 +0700

    CRUD User API with MongoDB

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = require("./routes");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 app.use(router);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

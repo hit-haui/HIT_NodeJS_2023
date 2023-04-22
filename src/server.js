74bda3ec4d77282010676491511d142c3951243d
luong2k3dev <tunhatluong2k3@gmail.com>
2023-04-22 17:38:55 +0700

    add file env.template, add port + db connect default and fix package.json

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(router);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/hit-nodejs')
   .then(() => console.log("Connect DB Successfully"))
   .catch(err => console.log(err));
 

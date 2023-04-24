29e34651bb21b1393949a30f43aa2fd128bf2e65
Nguyen-Duy-Son <nguyenduyson@28072003@email.com>
2023-04-25 01:00:14 +0700

    da fix + CRUD Users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,41 +1,49 @@
 const User = require("../models/user.model");
 const getUsers = (req, res) => {
-  const getUsers = User.find();
+  const users = User.find();
   res.json({
-    getUsers,
+    users,
   });
 };
 const getUserById = (req, res) => {
   const getUsers = User.find();
   const { userId } = req.params;
-  const getUser = getUsers.find((item) => item.id == userId);
-  if (getUser) {
-    res.json({ user });
+  const  user= getUsers.find((item) => item.id == userId);
+  if (user) {
+    res.json({ userById });
   } else {
     res.json({ message: "User not found" });
   }
 };
 const createUser = (req, res) => {
-  const userCreate = new User(req.body);
-  const newUsers = userCreate.save();
-  res.json({ newUsers });
+  const userCreated = new User(req.body);
+  const users = userCreated.save();
+  res.json({ users});
 };
 const updateUserById = (req, res) => {
   const getUsers = User.find();
-  const userUpdate = new User(req.body);
+  const userUpdated = new User(req.body);
   const { userId } = req.params;
-  const newUser = getUsers.map((item, index) => {
+  if(!userId){
+    res.json({
+      message:"Khong tim thay id!"
+    })
+  }
+  const newUsers = users.map((item, index) => {
     if (item.id == userId) {
-      return userUpdate;
+      return userUpdated;
     }
     return item;
   });
-  res.json({ newUser });
+  res.json({ newUsers });
 };
 const deleteUserById = (req, res) => {
-  const getUsers = User.find();
+  const users = User.find();
   const { userId } = req.params;
-  const Newusers = getUsers.filter((item) => item.id !== userId);
+  if(!userId){
+    res.json({message:"Id khong ton tai!"})
+  }
+  const usersNew = users.filter((item) => item.id !== userId);
   res.json({ Newusers });
 };
 module.exports = {

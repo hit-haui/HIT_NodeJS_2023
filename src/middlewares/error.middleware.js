ca38365e4064462e41e1f7718ba478c8b9758532
Huong <hdnguyenthuhuong1234@gmail.com>
2023-05-06 02:16:19 +0700

    middleware_CRUD_with_mongoose

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,7 +1,4 @@
 const errorMiddleware = (err, req, res, next) => {
-  if (!err) {
-    next();
-  }
   res.status(err.status || 500).json({
     message: err.message,
   });
---
+++ src/models/classroom.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const classroomSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    classCode: {
+      type: String,
+      require: true,
+    },
+    image: String,
+    startTime: Date,
+    endTime: Date,
+    schedule: String,
+    studentTotal: Number,
+    location: String,
+    leaders: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    supports: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    students: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+const Classroom = mongoose.model("Classroom", classroomSchema);
+module.exports = Classroom;

4bdb6e9577cde59878c7f471a0ff1b7da75405f0
luong2k3dev <tunhatluong2k3@gmail.com>
2023-05-19 22:03:09 +0700

    fix some errors

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,19 +6,13 @@ const login = async (req, res, next) => {
     try {
         const { studentCode, password } = req.body;
         const user = await User.findOne({ studentCode });
-        if (!user) {
-            const err = new Error("User not found!");
-            err.status = 404;
-            throw err;
-        }
-        const isPassword = await bcrypt.compare(password, user.password);
-        if (!isPassword) {
+        if (!user || !(await bcrypt.compare(password, user.password))) {
             const err = new Error("Student code or password is incorrect!");
-            err.status = 400;
+            err.status = 401;
             throw err;
         }
         const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
-        res.status(201).json({
+        res.status(200).json({
             message: "Login successfully!",
             token: token
         });
@@ -29,20 +23,20 @@ const login = async (req, res, next) => {
 };
 
 const register = async (req, res, next) => {
-    const newUser = req.body;
+    const { fullName, password, studentCode, className, schoolYear } = req.body;
     try {
-        const checkUser = await User.findOne({ studentCode: newUser.studentCode });
-        if (checkUser) {
-            const err = new Error('User already exists!');
-            err.status = 409;
-            throw err;
-        }
-        if (!newUser.studentCode) {
+        if (!studentCode) {
             const err = new Error('Student code is required!');
             err.status = 400;
             throw err;
         }
-        User.create(newUser);
+        const checkUser = await User.findOne({ studentCode });
+        if (checkUser) {
+            const err = new Error('User already exists!');
+            err.status = 400;
+            throw err;
+        }
+        User.create({ fullName, password, studentCode, className, schoolYear });
         res.status(201).json({ message: 'register successfully!' });
     }
     catch (err) {

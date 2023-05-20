e125aa8077dea4a7acd3112a4a3e581f97e5fe01
Huong <hdnguyenthuhuong1234@gmail.com>
2023-05-20 14:36:21 +0700

    API Register and Login_add auth.Middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,38 @@
-const authMiddleware = (req, res, next) => {
-    // In Postman use Query Params: [key, value] = [permission, admin]
-    let { permission } = req.query;
-    if (permission !== 'admin') {
-        const err = new Error('Unauthorized');
-        err.status = 401;
-        throw err;
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+const authMiddleware = async (req, res, next) => {
+  try {
+    console.log(req.headers);
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      const err = new Error("Unauthorized");
+      err.status = 401;
+      throw err;
     }
-    next();
-}
+
+    const token = authorization.split(" ")[1];
+    const payload = jwt.verify(token, process.env.SECRET_KEY);
+
+    const userId = payload.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      const err = new Error("Unauthorized");
+      err.status = 401;
+      throw err;
+    }
+    req.user = user;
+    if (user.role !== "admin") {
+      const err = new Error("Forbidden");
+      err.status = 403;
+      throw err;
+    }
+    res.json({
+      user,
+    });
+    return next();
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = authMiddleware;

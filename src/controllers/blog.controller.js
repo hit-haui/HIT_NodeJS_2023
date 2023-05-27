ac1d55f28857f7ccc5c03d8ad41c507ee429fff8
Huong <hdnguyenthuhuong1234@gmail.com>
2023-05-27 20:13:56 +0700

    ktra

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,5 +1,8 @@
 const Blog = require("../models/blog.model");
-const getBlogs = async (req, res, next) => {};
+const getBlogs = (req, res, next) => {
+  const blogs = Blog.find();
+  res.status(200).json({ blogs });
+};
 const getBlog = (req, res, next) => {};
 const createBlog = (req, res, next) => {};
 const updateBlog = (req, res, next) => {};
--- src/models/blog.model.js
+++ src/models/blog.model.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const BlogSchema = new Schema({});
+const BlogSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  data: {
+    type: String,
+  },
+});
 const Blog = mongoose.model("blog", BlogSchema);
 module.exports = {
   Blog,

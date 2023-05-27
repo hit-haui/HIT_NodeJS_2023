9643376c41b38c550496bc6eac771cd9722ab492
luong2k3dev <tunhatluong2k3@gmail.com>
2023-05-27 19:21:18 +0700

    Set up MongoDB

---
+++ src/models/blog.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+    {
+        title: {
+            type: String,
+            default: null
+        },
+        content: {
+            type: String,
+            default: null
+        },
+        author: {
+            type: String,
+            require: true
+        },
+        image: {
+            type: String,
+            default: null
+        },
+        comment: {
+            type: String,
+            default: null
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = require("./routes/index.route");
 const dotenv = require("dotenv");
-
-const app = express();
-const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.DB_URL || "mongodb://127.0.0.1:27017/hit-nodejs";
+
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connect database successfully!"))
+    .catch((err) => console.log(err));
 
 app.use(router);
 

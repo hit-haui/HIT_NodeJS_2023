30c476e1a92b20a23b6a2bae35a1e2f19fd255e6
caotrungduc <trungdc2003@gmail.com>
2023-05-27 18:54:59 +0700

    set up env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');

29412bbab1eedb1a32e6971e15215afb402107d8
Huong <hdnguyenthuhuong1234@gmail.com>
2023-05-28 22:27:42 +0700

    setup env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
+const port = process.env.PORT || 5000;
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(console.log("Example app listening on port 3000"));
+app.listen(console.log(`Example app listening on port ${port}`));

b377a2306025a3b83a906046689b85f5258f00e4
caotrungduc <trungdc2003@gmail.com>
2023-05-31 06:29:43 +0700

    set up mongoDB, model blog

---
+++ src/models/blog.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        topic: {
+            type: String,
+            default: 'Other'
+        },
+        author: {
+            type: String,
+            default: 'Anonymous'
+        },
+        views: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 8080;
+const mongoURI = process.env.DB_URI || "mongodb://127.0.0.1:27017/hit-nodejs";
+
+(async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connect database successfully!")
+    } catch (error) {
+        console.log(error);
+    }
+})();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');

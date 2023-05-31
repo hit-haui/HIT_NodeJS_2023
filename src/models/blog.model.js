5a860e64200ffb47e8e0911671c67519becb6bcc
lieu <dinnhonglieu3@gmail.com>
2023-06-01 02:20:50 +0700

    set up mongoDB - model Blog

---
+++ src/models/blog.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: String,
+            default: 'Guest'
+        },
+        reactions: {
+            type: Number,
+            default: 0
+        },
+        images: [{ type: String }],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 const app = express()
 
 dotenv.config()
 const port = process.env.PORT || 3000
+const mongoURI = process.env.URI_DB || "mongodb://127.0.0.1:27017/Blog_Test"
 
+mongoose
+.connect(mongoURI)
+.then(() => console.log('Connect database successfully!'))
+.catch((err) => {
+    console.log(err)
+})
 app.get("/", function(req, res) {
     res.send("Hello word")
 })

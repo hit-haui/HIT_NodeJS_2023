39bd646601e6b0550be738c6028ecff30b0f1963
lieu <dinnhonglieu3@gmail.com>
2023-06-01 01:49:04 +0700

    set up env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express")
-
+const dotenv = require("dotenv")
 const app = express()
+
+dotenv.config()
+const port = process.env.PORT || 3000
 
 app.get("/", function(req, res) {
     res.send("Hello word")
 })
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });

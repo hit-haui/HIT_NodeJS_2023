8d95548274c6cf6566e1c183331f400201b4b7af
= <=>
2023-05-31 21:40:25 +0700

    set up mongoDB + model blog

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -6,6 +7,14 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 8080;
+const mongoURI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/blog-default';
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log('Connect database successfully!'))
+  .catch((err) => {
+    console.log(err);
+});
 
 app.get('/', (req, res) => {
     res.send('HIT NodeJS 2023!');

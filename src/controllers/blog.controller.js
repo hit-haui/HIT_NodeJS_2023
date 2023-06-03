a6c4b5f24085f290dfacd2618c54c705b54a0b93
luong2k3dev <tunhatluong2k3@gmail.com>
2023-06-03 19:38:16 +0700

    upload image

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog.model');
+const multer = require('multer');
 
 const getBlogs = async (req, res, next) => {
     try {
@@ -27,19 +28,49 @@ const getBlog = async (req, res, next) => {
 };
 
 const createBlog = async (req, res, next) => {
+    const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/');
+        },
+        filename: (req, file, cb) => {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg');
+        }
+    });
+    const upload = multer({ storage });
     const newBlog = req.body;
     try {
+        console.log("blog: ", req);
         if (!newBlog.author) {
-            const err = new Error('Author is require!');
+            const err = new Error('Author is required!');
             err.status = 400;
             throw err;
         }
-        const blog = await Blog.create(newBlog);
-        res.status(201).json({ blog });
-    }
-    catch (err) {
+        upload.single('image')(req, res, (err) => {
+            if (err) {
+                err.status = 400;
+                throw err;
+            }
+            newBlog.image = req.file.filename;
+            const blog = Blog.create(newBlog);
+            res.status(201).json({ blog });
+        });
+    } catch (err) {
         next(err);
-    };
+    }
+    // const newBlog = req.body;
+    // try {
+    //     if (!newBlog.author) {
+    //         const err = new Error('Author is require!');
+    //         err.status = 400;
+    //         throw err;
+    //     }
+    //     const blog = await Blog.create(newBlog);
+    //     res.status(201).json({ blog });
+    // }
+    // catch (err) {
+    //     next(err);
+    // };
 };
 
 const updateBlogById = async (req, res, next) => {

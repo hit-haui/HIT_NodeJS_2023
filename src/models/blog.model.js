f305784b359770699a0de5fe80fdee67e35e3edb
Huong <hdnguyenthuhuong1234@gmail.com>
2023-06-07 00:24:05 +0700

    setup mongoose

---
+++ src/models/blog.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+});
+const Blog = mongoose.model("Blog", blogSchema);
+module.exports = Blog;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
-const port = process.env.PORT || 5000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+const port = process.env.PORT || 3000;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nodejs";
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log("Example app listening on port", `${port}`);

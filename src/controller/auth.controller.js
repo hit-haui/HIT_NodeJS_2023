4ad68ee9c38338cf47662220ecb3a66f23dba012
unknown <lelenahkht2003@gamil.com>
2023-06-10 19:04:09 +0700

    API login + secretKey JWT + create token + date of token

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -25,6 +25,36 @@ const Register = async (req, res, next) => {
   }
 };
 
+const Login = async (req, res, next) => {
+  const { name, password } = req.body;
+  try {
+    if (!password) {
+      const err = new Error("Password is required!");
+      err.status = 400;
+      throw err;
+    }
+    const checkUser = await User.findOne({ name });
+    const isPassword = await bcrypt.compare(password, checkUser.password);
+    if (!isPassword) {
+      const err = new Error("Password is incorrect");
+      err.status(401);
+      throw err;
+    }
+    const token = await jwt.sign(
+      { name: checkUser.name },
+      process.env.PRIVATE_KEY,
+      { expiresIn: process.env.DATE_OF_TOKEN }
+    );
+    res.status(200).json({
+      message: "Login successfully",
+      token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   Register,
+  Login,
 };
--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const { Register } = require("../controller/auth.controller");
+const { Register, Login } = require("../controller/auth.controller");
 
 const authRoute = express.Router();
 
 authRoute.route("/register").post(Register);
+authRoute.route("/login").post(Login);
 
 module.exports = authRoute;

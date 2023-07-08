e3db9e1850ae54a129b8b6fa2f227b5dc4461472
haniekid <thuharosy2003@gmail.com>
2023-07-08 17:35:33 +0700

    filter, export api data into excel file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,33 @@ const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
 const ApiError = require("../utils/ApiError");
 const httpStatus = require("http-status");
+const reader = require("xlsx");
 
 const getUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-  console.log(users);
+  const { page } = req.query;
+
+  // pagination
+  const limit = 2;
+  const skip = (page - 1) * limit;
+
+  // plain JS object
+  const users = await User.find({ username: "hung", age: 20 })
+    .select("-createdAt -updatedAt -__v -password -avatar")
+    .limit(limit)
+    .skip(skip)
+    .lean();
+  if (!users) throw new ApiError(httpStatus.NOT_FOUND, "User not found!");
+
+  // Reading our test file
+  const file = reader.readFile("../data/test.xlsx");
+
+  const ws = reader.utils.json_to_sheet(users);
+
+  reader.utils.book_append_sheet(file, ws, "Sheet3");
+
+  // Writing to our file
+  reader.writeFile(file, "../data/test.xlsx");
+
   res.status(httpStatus.OK).json({ users });
 });
 
@@ -14,11 +37,11 @@ const getUser = catchAsync(async (req, res) => {
   const user = await User.findById(userId);
   if (!user) throw new ApiError(httpStatus.NOT_FOUND, "User not found!");
   user.getAge();
-  res.status((httpStatus.OK)).json({
+  res.status(httpStatus.OK).json({
     code: httpStatus.OK,
-    message: 'Get users successfully',
+    message: "Get users successfully",
     data: user.toJSON({ virtuals: true }),
-  })
+  });
   // res.status(httpStatus.OK).json({ user: user.toJSON({ virtuals: true }) });
 });
 
